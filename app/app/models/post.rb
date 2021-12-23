b409fef68150a2d000233932d3a5b9708ec3d758
Yuki Tojima <yuki@haizakura.com>
2021-12-23 12:16:32 +0900

    [Add] コメント

--- app/app/models/post.rb
+++ app/app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   def self.search(keyword)
+    # あいまい検索
+    #   “?”に対してkeywordが順番に入る
+    #   LIKEは、あいまい検索の意味で、“%”は、前後のあいまいという意味
+    #   “#{keyword}”は、Rubyの式展開
     where('title LIKE ? OR body LIKE ?', "%#{keyword}%", "%#{keyword}%")
   end
 end
